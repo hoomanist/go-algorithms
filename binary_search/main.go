a05ece89f03569f45bc8b344f152bfb8483804f2
hoomanist <hham.group12@gmail.com>
2020-10-14 14:57:15 +0330

    better error handeling

--- binary_search/main.go
+++ binary_search/main.go
@@ -2,27 +2,33 @@ package main
 
 import (
   "fmt"
+  "os"
+
 )
 
 
-func Search(Target int, data []int) int {
+func Search(Target int, data []int) (int, error) {
   StartIndex := 0
   EndIndex := len(data)-1
   for StartIndex != EndIndex {
     Mid := (StartIndex + EndIndex) / 2
     if data[Mid] == Target {
-      return Mid
+      return Mid, nil
     }else if data[Mid] > Target {
       EndIndex = Mid
     }else if data[Mid] < Target {
       StartIndex = Mid
     }
   }
-  return -1
+  return -1, fmt.Errorf("No such number")
 }
 
 func main(){
   data := []int{1, 2, 3, 4, 5, 6, 7}
-  res := Search(4, data)
+  res, err := Search(6, data)
+  if err != nil {
+    fmt.Println("not found")
+    os.Exit(0)
+  }
   fmt.Println(res)
 }
